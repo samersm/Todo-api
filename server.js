234102565a122c1274d94fea8122aface6508dcf
Samer Zagzoog <mrsamer.dev@gmail.com>
2016-07-30 05:28:01 +0000

    Add POST for todos

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 8080;
-var todos = [{
-    id: 1,
-    description: 'Eat pizza for lunch',
-    completed: false
-}, {
-    id: 2,
-    description: 'Go to market',
-    completed: false
-}, {
-    id: 3,
-    description: 'Get some ice cream',
-    completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send('Todo API Root');
@@ -47,6 +40,19 @@ app.get('/todos/:id', function (req, res) {
     
     // end of challenge code
     
+});
+
+// POST /todos
+app.post('/todos', function (req, res) {
+    var body = req.body;
+    
+    // add id field
+    body.id = todoNextId++;
+    
+    // push body into array
+    todos.push(body);
+    
+    res.json(body);
 });
 
 app.listen(PORT, function () {
