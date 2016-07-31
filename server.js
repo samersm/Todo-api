1945e9fac151022c7c513a40c3bf978e675877de
Samer Zagzoog <mrsamer.dev@gmail.com>
2016-07-31 04:47:49 +0000

    Add PUT todos/:id

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.post('/todos', function (req, res) {
 });
 
 // DELETE /todos/:id
-    app.delete('/todos/:id', function (req, res) {
+app.delete('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
     var matchedTodo = _.findWhere(todos, {id: todoId});
     
@@ -63,7 +63,36 @@ app.post('/todos', function (req, res) {
         res.json(matchedTodo);
     }
     
-    });
+});
+    
+// PUT /todos/:id
+app.put('/todos/:id', function (req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    var body = _.pick(req.body, 'description', 'completed');
+    var validAttributes = {};
+    
+    if (!matchedTodo) {
+        return res.status(404).send();
+    }
+    
+    //    body.hasOwnProperty('completed');
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+    } else if (body.hasOwnProperty('completed')) {
+        return res.status(400).send();
+    }
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+       validAttributes.description = body.description;
+    } else if (body.hasOwnProperty('description')) {
+        return res.status(400).send();
+    }
+    
+// HERE 
+    _.extend(matchedTodo, validAttributes);
+    res.json(matchedTodo);
+
+});
 
 
 app.listen(PORT, function () {
