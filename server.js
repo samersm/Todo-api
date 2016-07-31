0b70da619ee411da32bdb0433471c3c4effaf170
Samer Zagzoog <mrsamer.dev@gmail.com>
2016-07-31 08:15:17 +0000

    Final static todos

--- server.js
+++ server.js
@@ -18,12 +18,16 @@ app.get('/todos', function (req, res) {
     var queryParams = req.query;
     var filteredTodos = todos;
     
-    // if has property && completed === "true"
-    //      filteredTodos = _.where(filteredTodos, ?);
     if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
        filteredTodos = _.where(filteredTodos, {completed: true});
     } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
        filteredTodos = _.where(filteredTodos, {completed: false});
+    }
+    
+    if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+       filteredTodos = _.filter(filteredTodos, function(todo){ 
+           return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+      });
     }
     
     res.json(filteredTodos);
