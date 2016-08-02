395defc9e364f316c22a1898638a7b69ea89f32a
Samer Zagzoog <mrsamer.dev@gmail.com>
2016-08-02 07:46:09 +0000

    Add todo model and POST /todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -51,6 +52,13 @@ app.get('/todos/:id', function (req, res) {
 app.post('/todos', function (req, res) {
     var body = _.pick(req.body, 'description', 'completed');
     
+    db.todo.create(body).then(function (todo) {
+           res.json(todo.toJSON());
+        },function (e) {
+            res.status(400).json(e);
+    });
+    
+    /*
         if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
         return res.status(400).send();
     }
@@ -64,6 +72,7 @@ app.post('/todos', function (req, res) {
     todos.push(body);
     
     res.json(body);
+    */
 });
 
 // DELETE /todos/:id
@@ -109,7 +118,9 @@ app.put('/todos/:id', function (req, res) {
 
 });
 
-
-app.listen(PORT, function () {
+db.sequelize.sync().then(function () {
+    app.listen(PORT, function () {
     console.log('Express listening on port ' + PORT + '!');
+    });
 });
+
