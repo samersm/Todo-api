340509c4db4dbdf04786e6c318ab96bb9e1a4fcc
Samer Zagzoog <mrsamer.dev@gmail.com>
2016-08-05 16:08:03 +0000

    Add login route and authenticate class method

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcrypt = require('bcrypt');
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -117,6 +118,7 @@ app.put('/todos/:id', function (req, res) {
     });
 });
 
+// POST /users
 app.post('/users', function (req, res) {
     
     var body = _.pick(req.body, 'email', 'password');
@@ -128,7 +130,19 @@ app.post('/users', function (req, res) {
     });    
 });
 
-db.sequelize.sync().then(function () {
+// POST /user/login
+app.post('/users/login', function (req, res) {
+    var body = _.pick(req.body, 'email', 'password');
+    
+    db.user.authenticate(body).then(function (user) {
+        res.json(user.toPublicJSON());
+    }, function () {
+        res.status(401).send();
+    });
+    
+});
+
+db.sequelize.sync({force: true}).then(function () {
     app.listen(PORT, function () {
     console.log('Express listening on port ' + PORT + '!');
     });
